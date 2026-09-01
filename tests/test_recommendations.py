06a1ba5cec888ff328456c1cccaf6974c228006b
Shreyashree Mondal <shreyashreemanai@gmail.com>
2026-08-31 19:50:52 -0500

    Add recommendation scoring tests

--- tests/test_recommendations.py
+++ tests/test_recommendations.py
@@ -1,1 +1,66 @@
-Add recommendation scoring tests
+import pandas as pd
+
+from dbmt_project_frontend_cloud import calculate_recommendation_score
+
+
+def test_recommendation_score_is_between_zero_and_one():
+    row = pd.Series({
+        "energy": 80,
+        "danceability": 70,
+        "valence": 75,
+        "acousticness": 30,
+        "popularity": 90,
+        "user_rating": 5
+    })
+
+    score = calculate_recommendation_score(
+        row,
+        avg_energy_n=0.8,
+        avg_dance_n=0.7,
+        avg_valence_n=0.75,
+        avg_acoustic_n=0.3
+    )
+
+    assert 0.0 <= score <= 1.0
+
+
+def test_perfect_audio_match_produces_high_score():
+    row = pd.Series({
+        "energy": 80,
+        "danceability": 70,
+        "valence": 75,
+        "acousticness": 30,
+        "popularity": 100,
+        "user_rating": 5
+    })
+
+    score = calculate_recommendation_score(
+        row,
+        avg_energy_n=0.8,
+        avg_dance_n=0.7,
+        avg_valence_n=0.75,
+        avg_acoustic_n=0.3
+    )
+
+    assert score == 1.0
+
+
+def test_missing_audio_features_use_defaults():
+    row = pd.Series({
+        "energy": None,
+        "danceability": None,
+        "valence": None,
+        "acousticness": None,
+        "popularity": 50,
+        "user_rating": None
+    })
+
+    score = calculate_recommendation_score(
+        row,
+        avg_energy_n=0.5,
+        avg_dance_n=0.5,
+        avg_valence_n=0.5,
+        avg_acoustic_n=0.5
+    )
+
+    assert 0.0 <= score <= 1.0
